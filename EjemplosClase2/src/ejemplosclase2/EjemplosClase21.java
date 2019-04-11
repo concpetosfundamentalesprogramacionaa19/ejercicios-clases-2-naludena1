3d5e5e225ebe3033053bb9517597bfe9d4cae324
naludena1 <nohealejandra7@gmail.com>
2019-04-11 10:50:56 -0500

    ejemplos-clases-2-naludena1/

---
+++ EjemplosClase2/src/ejemplosclase2/EjemplosClase21.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosclase2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Salas
+ */
+public class EjemplosClase21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+       String nombre; 
+       String apellido;
+       Scanner entrada = new Scanner(System.in);
+       
+        System.out.println("Por favor ingrese sus nombres");
+       nombre = entrada.nextLine();
+        
+        
+        
+        System.out.println("Por favor ingrese su apellido");
+       apellido = entrada.nextLine();
+       
+     
+ 
+    
+    }
+    
+}
